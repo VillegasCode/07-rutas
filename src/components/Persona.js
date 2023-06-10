cd00cbca0f32a4f701898353218b782ff6f32076
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-09 19:40:27 -0500

    We import useParams from react-router-dom and we use 2 props through destructuring named nombre and apellido

--- src/components/Persona.js
+++ src/components/Persona.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export const Persona = () => {
+
+const {nombre, apellido} = useParams();
+
   return (
     <div>
-        <h1>Página de PERSONA</h1>
+        <h1>Página de PERSONA: {nombre} {apellido}</h1>
         <p>Esta página es de Persona</p>
     </div>
   )
