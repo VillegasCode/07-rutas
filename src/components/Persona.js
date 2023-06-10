67df1c12203a6366a82638a2de389427b5cda99d
Carlos Villegas <carlos.villegasch@gmail.com>
2023-06-09 20:48:07 -0500

    We create a hook useNavigate with const navegar and create a function named enviar to get data from a form

--- src/components/Persona.js
+++ src/components/Persona.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export const Persona = () => {
 
@@ -14,14 +14,38 @@ let {nombre, apellido} = useParams();
 //     apellido = "";
 // }
 
+
+const navegar = useNavigate();
+
+const enviar = (e) => {
+    e.preventDefault();
+    let nombre = e.target.nombre.value;
+    let apellido = e.target.apellido.value;
+    let url = `/persona/${nombre}/${apellido}`;
+
+    if (nombre.length <= 0 && apellido.length <= 0){
+        navegar("/inicio");
+    } else if (nombre === "contacto"){
+        navegar("/contacto");
+    } else {
+        navegar(url);
+    }
+};
+
   return (
     <div>
 
-{/* Condicionales ternarios */}
-{!nombre && <h1>No hay ninguna persona que mostrar</h1>}
-{nombre && <h1>Página de persona: {nombre} {apellido}</h1>}
+        {/* Condicionales ternarios */}
+        {!nombre && <h1>No hay ninguna persona que mostrar</h1>}
+        {nombre && <h1>Página de persona: {nombre} {apellido}</h1>}
 
         <p>Esta página es de Persona</p>
+
+        <form onSubmit={enviar}>
+            <input type='text' name='nombre' />
+            <input type='text' name='apellido' />
+            <input type='submit' name='enviar' value="enviar" />
+        </form>
     </div>
   )
 }
